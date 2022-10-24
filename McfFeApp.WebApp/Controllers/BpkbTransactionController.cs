9cfab84194cd437228e1a9b0df7b693238fc8587
firmanherdiansyah <herdiansyah.fh.firman@gmail.com>
2022-10-24 15:50:25 +0700

    add controller

--- McfFeApp.WebApp/Controllers/BpkbTransactionController.cs
+++ McfFeApp.WebApp/Controllers/BpkbTransactionController.cs
@@ -1,12 +1,56 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using McfFeApp.WebApp.Dto;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace McfFeApp.WebApp.Controllers
 {
     public class BpkbTransactionController : Controller
     {
-        public IActionResult Index()
+
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public BpkbTransactionController(IHttpClientFactory httpClientFactory)
         {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        private async Task<T?> InvokeEndpoint<T>(string clientName, string url)
+        {
+            BpkpTransaction? bpkbTransaction;
+
+            var httpClient = httpClientFactory.CreateClient(clientName);
+
+            return await httpClient.GetFromJsonAsync<T>(url);
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            using var httpClient = httpClientFactory.CreateClient("McfAPI");
+
+            var getResult = await httpClient.GetFromJsonAsync<List<StorageLocation>>("StorageLocation");
+
+            ViewBag.Location = getResult.Select(l => new SelectListItem { Text = l.LocationName, Value = l.LocationId });
+
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(BpkpTransaction bpkpTransaction)
+        {
+            if (ModelState.IsValid) 
+            {
+                using var httpClient = httpClientFactory.CreateClient("McfAPI");
+
+                var result = await httpClient.PostAsJsonAsync("bpkptransaction", bpkpTransaction );
+
+                result.EnsureSuccessStatusCode();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+
+
     }
 }
--- McfFeApp.WebApp/Dto/BpkpTransaction.cs
+++ McfFeApp.WebApp/Dto/BpkpTransaction.cs
@@ -1,27 +1,45 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace McfFeApp.WebApp.Dto
 {
     public class BpkpTransaction
     {
         [StringLength(100)]
+        [DisplayName("Agrrement Number")]
         public string? AgrrementNumber { get; set; }
 
+        [StringLength(10)]
+        [DisplayName("Branch Id")]
+        public string? BranchId { get; set; }
+
         [StringLength(100)]
+        [DisplayName("No. Bpkb")]
         public string? BpkbNumber { get; set; }
 
+        [DisplayName("Tgl. Bpkp")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime BpkbDate { get; set; }
 
+        [StringLength(100)]
+        [DisplayName("No. Faktur")]
+        public string? FakturNumber { get; set; }
+
+        [DisplayName("Tanggal Faktur")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime FakturDate { get; set; }
 
         [StringLength(10)]
+        [DisplayName("Lokasi Penyimpanan")]
         public string? LocationId { get; set; }
-        public StorageLocation? StorageLocation { get; set; }
 
+        [DisplayName("No. Polisi")]
         [StringLength(20)]
         public string? PoliceNumber { get; set; }
 
+        [DisplayName("Tgl. Bpkp In")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime BpkbDateIn { get; set; }
     }
 
